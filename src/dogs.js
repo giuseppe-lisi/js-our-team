a0794bbf2c1015ab00f74ab1818d16845b2a7d66
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2025-11-19 20:01:36 +0100

    i cani sono ora mostrati a schermo quando si clicca il bottone

--- src/dogs.js
+++ src/dogs.js
@@ -1,6 +1,15 @@
 const url = 'https://api.thedogapi.com/v1/images/search?limit=10';
 const loadMore = document.getElementById('load');
 
+function printDogs(dogImgs){
+    const dogContainer = document.getElementById('dog-img-container');
+
+    for (let i = 0; i < dogImgs.length; i++) {
+        const dogImg = dogImgs[i];
+        dogContainer.innerHTML += `<img src="${dogImg}">`
+    }
+}
+
 function getImgFromArrOfObj(array) {
     const dogImgUrls = [];
 
@@ -9,19 +18,15 @@ function getImgFromArrOfObj(array) {
         dogImgUrls.push(dogObj.url);
     }
     console.log(dogImgUrls);
-    return dogImgUrls.join();
-
+    return dogImgUrls;
 }
 
 loadMore.addEventListener('click', () => {
     fetch(url)
+        // fetch dell'array con i cani e parsa in json
         .then(response => response.json())
-        .then(dogsArray => getImgFromArrOfObj(dogsArray))
+        .then(dogsArray => printDogs(getImgFromArrOfObj(dogsArray)))
+        .then(logThis => console.log(logThis))
         .catch(error => console.error(error));
 });
-
-
-
-
-
 
